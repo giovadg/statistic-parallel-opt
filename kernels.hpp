2a3e94db412d42741ca7aaa2ef9056c43c9b141c
unknown <giovanni.digiannatale@epfl.ch>
2025-12-31 18:20:32 +0100

    insert new parallelisms kind for benchmarks

--- kernels.hpp
+++ kernels.hpp
@@ -6,7 +6,7 @@ using namespace std;
 
 namespace kernels {
 
-void rolling_mean_serial(vector<double> &arr_in, vector<double> &arr_out, size_t &w);
+void rolling_mean_serial(const vector<double> &arr_in, vector<double> &arr_out, size_t &w);
 
 // struct ThreadArgs {
 //     const vector<double>* arr_in;
@@ -17,5 +17,11 @@ void rolling_mean_serial(vector<double> &arr_in, vector<double> &arr_out, size_t
 // };
 
 void rolling_mean_parallel(vector<double> &arr_in, vector<double> &arr_out, size_t &w,  int num_threads);
+
+
+void rolling_mean_parallel_inputs(vector<vector<double>> &arrs_in, vector<vector<double>> &arrs_out,
+                                     size_t &w, int num_threads, bool nested_threads);
+
+
 
 };
--- main.cpp
+++ main.cpp
@@ -15,13 +15,18 @@ using namespace std;
 int main(int argc, char** argv) {
   size_t n         = (argc > 1) ? stoull(argv[1]) : 5000;
   int num_threads  = (argc > 2) ? stoull(argv[2]) : 1;
-  size_t w         = (argc > 3) ? stoull(argv[3]) : n/5;
-  int    Ntest_speed = (argc > 4) ? stoull(argv[4]) : 1;
+  size_t w         = (argc > 4-1) ? stoull(argv[4-1]) : n/5;
+  int    Ntest_speed = (argc > 5-1) ? stoull(argv[5-1]) : 1;
 
   vector<double> x1(n);
   vector<double> roll_av_x1_ser(n), roll_av_x1_pll(n); 
   vector<double> x2(n);
   vector<double> roll_av_x2_ser(n), roll_av_x2_pll(n); 
+  unordered_map<string,double> timings;
+  string method;
+  
+  vector<vector<double>> x_tot;
+  vector<vector<double>> roll_av_xtot_pll(2,vector<double>(n));
   
   boost::mt19937 rng1(123);
   boost::mt19937 rng2(100);
@@ -38,6 +43,10 @@ int main(int argc, char** argv) {
     x1[i] = initial_velocities_generator1();
     x2[i] = initial_velocities_generator2();
   }
+  x_tot.emplace_back(x1);
+  x_tot.emplace_back(x2);
+  roll_av_xtot_pll.emplace_back(roll_av_x1_pll);
+  roll_av_xtot_pll.emplace_back(roll_av_x2_pll);
 
   for (int j = 0; j < Ntest_speed; j++){
     auto start = chrono::high_resolution_clock::now();
@@ -45,29 +54,64 @@ int main(int argc, char** argv) {
     kernels::rolling_mean_serial(x2, roll_av_x2_ser, w);
     auto end  = chrono::high_resolution_clock::now();
     auto diff = chrono::duration<double>(end-start).count();
+    method = "serial vectors input - serial vector treatment:";
+    if (timings.find(method) != timings.end()){
+      timings[method] += diff;
+    }else{
+      timings[method] = diff;
+    }
+    
 
-    printf("serial: %f [s] \n",diff);
-
-    {
     start = chrono::high_resolution_clock::now();
     kernels::rolling_mean_parallel(x1, roll_av_x1_pll, w, num_threads);
     kernels::rolling_mean_parallel(x2, roll_av_x2_pll, w, num_threads);
     end  = chrono::high_resolution_clock::now();
     diff = chrono::duration<double>(end-start).count();
-    printf("parallel: %f [s] \n",diff);
+    method = "serial vectors input - parallel vector treatment:";
+
+    if (timings.find(method) != timings.end()){
+      timings[method] += diff;
+    }else{
+      timings[method] = diff;
     }
 
+    
+    for (bool nested_threads : {true,false}){
+    start = chrono::high_resolution_clock::now();
+    kernels::rolling_mean_parallel_inputs(x_tot, roll_av_xtot_pll, w, num_threads, nested_threads);
+    end  = chrono::high_resolution_clock::now();
+    diff = chrono::duration<double>(end-start).count();
+    if (nested_threads){
+      method="parallel vectors input - parallel vector treatment:";// %f [s] \n",diff);
+    }else{
+      method="parallel vectors input - serial vector treatment:";// %f [s] \n",diff);
+    }
+    if (timings.find(method) != timings.end()){
+      timings[method] += diff;
+    }else{
+      timings[method] = diff;
+    }
+    
+    }
   };
+  for (auto it=timings.begin(); it != timings.end();it++){
+    cout << it->first <<" "<< it->second<<" [s]"<<endl;
+    cout <<" "<<endl;
+  }
+
   // quick sanity check
-  vector<double> max_abs(2);
+  vector<double> max_abs(4);
   for (size_t i = 0; i < n; ++i) {
     if (isnan(roll_av_x1_ser[i]) && isnan(roll_av_x1_pll[i])) continue;
     max_abs[0] = max(max_abs[0], abs(roll_av_x1_ser[i] - roll_av_x1_pll[i]));
     if (isnan(roll_av_x2_ser[i]) && isnan(roll_av_x2_pll[i])) continue;
     max_abs[1] = max(max_abs[1], abs(roll_av_x2_ser[i] - roll_av_x2_pll[i]));
+    max_abs[2] = max(max_abs[2], abs(roll_av_x1_ser[i] - roll_av_xtot_pll[0][i]));
+    max_abs[3] = max(max_abs[3], abs(roll_av_x2_ser[i] - roll_av_xtot_pll[1][i]));
 
   }
-  cout << "max_abs_diff_1: " << max_abs[0] <<  "\nmax_abs_diff_2: " << max_abs[1] <<"\n";
+  cout <<"comparison between serial and single layer parallelism: \nmax_abs_diff_1: " << max_abs[0] <<  "\nmax_abs_diff_2: " << max_abs[1] <<"\n";
+  cout << "comparison between serial and nested parallelism: \nmax_abs_diff_1: " << max_abs[2] <<  "\nmax_abs_diff_2: " << max_abs[3] <<"\n";
 
   return 0;
 }
